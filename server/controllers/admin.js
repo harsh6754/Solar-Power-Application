d8e93d616e9bf5ef533e61fcb4fb9353ce29a6e6
harsh6754 <agarwalharsh6583@gmail.com>
2024-10-09 17:10:23 +0530

    Admin Routes

---
+++ server/controllers/admin.js
@@ -0,0 +1,5 @@
+const Admin = require('../models/admin');
+const errorHandler = require('../utils/ErrorsHandling');
+const bcryptjs = require('bcryptjs');
+// const exportTOXLSX  = require('../excel/exportToXLSX')
+
---
+++ server/models/admin.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.SchemaTypes({
+    username:{
+        tyep:String,
+        required:true,
+        unique:true,
+    },
+    mobile:{
+        type:Number,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    avatar:{
+        type:String,
+        required:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+},
+{timestamps:true}
+);
+
+const Admin = mongoose.model('Admin',adminSchema);
+
+export default Admin;
