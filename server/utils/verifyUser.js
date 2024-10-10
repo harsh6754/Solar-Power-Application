003b26941f59543f2dc65f685fd838dff6dc8474
harsh6754 <agarwalharsh6583@gmail.com>
2024-10-10 16:17:37 +0530

    Verify User Function

---
+++ server/utils/verifyUser.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwentoken';
+import ErrorsHandling from './ErrorsHandling';
+
+export const verifyToken = (req,res,next)=>{
+    const token = req.cookie.access_token;
+
+    if(!token) return next(ErrorsHandling(401,'Unauthorized'));
+
+    jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
+        if(err) return next(ErrorsHandling(403,'Forbidden || toke is not valid'));
+
+        req.user = user;
+        next();
+    });
+};
