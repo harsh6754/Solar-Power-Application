fb41c38dd72367acacf95d4d89a6121052ac5756
harsh6754 <agarwalharsh6583@gmail.com>
2024-10-11 15:10:33 +0530

    User Models Ready

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+const UserSchem = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    mobile:{
+        type:Number,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    plantname:{
+        type:String,
+        required:true,
+    },
+    address:{
+        type:String,
+    },
+},
+{timestamps:true}
+)
+
+const User = mongoose.model("User,UserSchema");
+
+export default User;
