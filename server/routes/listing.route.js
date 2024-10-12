40623b6ade336a85400ea27cf47873c93379fb1a
harsh6754 <agarwalharsh6583@gmail.com>
2024-10-12 08:58:39 +0530

    Listing Data Routes Ready

---
+++ server/routes/listing.route.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+    createList,
+    deleteListing,
+    getAllListings,
+    getOneList,
+    updateListing
+} from '../controllers/listining.controller.js';
+
+const router = express.Router();
+
+router.post("/createlist", createList);
+router.get("/getalllistings", getAllListings);
+router.get("/getonelist/:email", getOneList);
+router.get("/update/:id", updateListing);
+router.delete("/delete/:id", deleteListing);
+export default router;
+
