fb8cb25531201a179e3169a58990f88cd688bfcc
harsh6754 <agarwalharsh6583@gmail.com>
2024-10-12 21:44:29 +0530

    Check Password is strong or Week

--- server/models/admin.model.js
+++ server/models/admin.model.js
@@ -1,11 +1,20 @@
-//Admin models
-
 import mongoose from 'mongoose';
 
-// Correctly define the admin schema using mongoose.Schema
+// Function to check if a password is strong
+const isStrongPassword = (password) => {
+    const minLength = password.length >= 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChars = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    return minLength && hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChars;
+};
+
+// Define the admin schema
 const adminSchema = new mongoose.Schema({
     username: {
-        type: String, // Corrected 'tyep' to 'type'
+        type: String,
         required: true,
         unique: true,
     },
@@ -21,12 +30,18 @@ const adminSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+        validate: {
+            validator: function (password) {
+                return isStrongPassword(password);
+            },
+            message: props => `Password should be at least 8 characters long, contain uppercase, lowercase, numbers, and special characters!`,
+        },
     },
     avatar: {
         type: String,
-        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png", // Use 'default' for a fallback value
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
     },
-}, 
+},
 { timestamps: true });
 
 // Create the Admin model
