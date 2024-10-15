2594e09dc57e00a22fbbcf4829de666f0fe9344f
harsh6754 <agarwalharsh6583@gmail.com>
2024-10-15 07:40:38 +0530

    Header And Footer Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import Header from './component/Header';
+import Footer from './component/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Header />
       <App />
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
